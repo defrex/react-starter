2f41e4c635cbe706b549aa00b95afdb69361d159
Aron Jones <aron.jones@gmail.com>
2015-10-01 10:11:44 -0400

    remove commented code

--- client.js
+++ client.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from 'react-router';
-// import NProgress from 'nprogress';
 
 import routes from './routes.js';
 
@@ -11,10 +10,3 @@ ReactDOM.render(
   <Router>{routes}</Router>,
   document.getElementById('root')
 );
-
-// Router.run(routes, Router.HistoryLocation, function(Handler, state) {
-//   NProgress.start();
-//   React.render(<Handler params={state.params}/>, document.getElementById('root'));
-//   NProgress.done();
-//   window.ga('send', 'pageview');
-// });
